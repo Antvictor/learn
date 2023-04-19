3d700493bbfd80a4b6478f1920a1b87c13c14a57
Antvictor <larryyangc@163.com>
2023-04-19 11:59:57 +0800

    feat: consumer完成，但最终请求依旧使用的是ip地址，需要再研究下

---
+++ nacos-learn/spring-cloud-nacos-consumer/src/main/java/com/antvictor/ConsumerApplication.java
@@ -0,0 +1,27 @@
+package com.antvictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author exccedy
+ * @date 2023/4/19
+ **/
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConsumerApplication {
+
+    @Bean
+//    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
---
+++ nacos-learn/spring-cloud-nacos-consumer/src/main/java/com/antvictor/config/ConsumerConfig.java
@@ -0,0 +1,15 @@
+package com.antvictor.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author exccedy
+ * @date 2023/4/19
+ **/
+@Configuration
+public class ConsumerConfig {
+
+}
